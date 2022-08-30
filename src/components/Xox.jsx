c645d92cfb11f7c28dc61b5b3f850400460f63eb
Uygar Süslü <uygarsuslu1@gmail.com>
2022-08-30 17:24:56 +0300

    draw check

--- src/components/Xox.jsx
+++ src/components/Xox.jsx
@@ -4,13 +4,36 @@ import Square from "./Square";
 const INITIAL = "";
 const X_PLAYER = "X";
 const O_PLAYER = "O";
+const winCombination = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function Xox() {
   const [grid, setGrid] = useState(Array(9).fill(INITIAL));
   const [player, setPlayer] = useState(false);
+  const [gameFinished, setGameFinished] = useState(false);
+  const [draw, setDraw] = useState(false);
+
+  function isGameOver() {
+    if (!gameFinished) {
+      // DRAW CHECK
+      if (!grid.includes(INITIAL)) {
+        setDraw(true);
+        setGameFinished(true);
+      }
+    }
+  }
+
+  isGameOver();
 
   function handleClick(id) {
-    console.log("inside handleClick");
     setGrid(
       grid.map((item, index) => {
         if (index === id) {
